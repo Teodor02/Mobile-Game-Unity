56d940a2e534f8fa35f5fe52894ab71aaa2c7021
TeoDur <47451951+TeoDur@users.noreply.github.com>
2019-02-17 16:53:12 +0100

    Jävla hoppscript

---
+++ Mobile Game/Assets/Scripts/Ball/Jump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+
+    public bool inAir = false;
+    float Speed = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (inAir == false)
+            {
+                transform.position += transform.up * Time.deltaTime * Speed;
+                inAir = true;
+            }
+        }
+    }
+    void OnCollisionEnter(Collision hit)
+        {
+            if (hit.gameObject.tag == "Ground")
+            {
+                inAir = false;
+            }
+        }
+}
